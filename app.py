dcfb6190f13706a7c040acf33d839962e7e0f920
SaraHM0330 <160879031+SaraHM0330@users.noreply.github.com>
2024-02-22 10:13:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,3 +3,7 @@ from PIL import Image
 
 st.title(" Bienvenido a Sara,s Site")
 
+st.header("En este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales")
+st.Write("Facilmente puedo realizar backend y frontend.")
+
+
