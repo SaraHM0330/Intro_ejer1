af2023ebc349cacf9ddb8b1fd5d51425c82f51bc
SaraHM0330 <160879031+SaraHM0330@users.noreply.github.com>
2024-02-22 10:44:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,5 +7,36 @@ st.header("En este espacio comienzo a desarrollar mis aplicaciones para interfac
 st.write("Facilmente puedo realizar backend y frontend.")
 image=Image.open("flores.jpg")
 st.image(image, caption= "Interfaces Multimodales Sara")
+
+texto=st.text_input("Escribe algo", "Este es mi texto")
+st.write("el texto escrito es", texto)
+
+st.subheader("Ahora Usemos 2 columnas")
+col1, col2 = st.columns(2)
+
+with col2:
+  st.subheader("Esta es la segunda")
+  modo=st.radio("Que modalidad es la principal de tu interfaz",("Visual", "auditivo", "tactil"))
+  if modo=="Visual":
+    st.write("lA VISTA ES FUNDAMENTAL PARA TU INTERFAZ")
+  if modo=="auditivo":
+    st.write("skjasjdklas")
+  if modo=="tactil":
+    st.write("jdasdlaksdadoj")
+
+with col1:
+  st.subheader("Esta es la primera columna")
+  st.write("Las interfaces multimodales mejoran la experiencia de usuario")
+  resp=st.checkbox("Estoy de acuerdo")
+  if resp:
+    st.write("correcto!) 
+
+st.subheader("uso de botones")
+             
+
+
+
+  
+
                  
 
